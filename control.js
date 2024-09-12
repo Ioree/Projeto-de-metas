bc6ecee351b8d33ede78faaecdc6f2aed0d143ab
Iore <ioresilvarock@gmail.com>
2024-09-12 19:53:03 -0300

    listar metas

--- control.js
+++ control.js
@@ -1,4 +1,4 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
 
 let meta = {
     value: 'Tomar agua todo dia',
@@ -18,6 +18,35 @@ const cadastrarMeta = async () => {
     metas.push(
         { value: meta, checked: false }
     )
+
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para alterna entre as metas, o Espaço para selecionar e o Enter para confirmar",
+        choices: [...metas],
+        instructions: false,
+    })
+
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) concluida)(s)')
 
 }
 
@@ -50,7 +79,7 @@ const start = async () => {
                 console.log(metas)
                 break
             case "listar":
-                console.log("vamos listar")  
+                await listarMetas()
                 break
             case "sair":
                 console.log("Saindo...")
